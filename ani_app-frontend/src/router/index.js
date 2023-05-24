d3e23253c887dd53346be6b652b4b0efaf7ba320
genken <2024genken.gf@student.letovo.ru>
2023-05-25 01:45:41 +0300

    added index

--- ani_app-frontend/src/router/index.js
+++ ani_app-frontend/src/router/index.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import Index from '@/views/Index.vue';
 import HelloWorld from '@/components/testing/HelloWorld.vue';
-import Index from '@/components/testing/BootstrapTest.vue';
+import BootstrapTest from '@/components/testing/BootstrapTest.vue';
 
 export default createRouter({
     history: createWebHistory(),
     routes: [
         { name: 'index', path: '/', component: Index },
+        { name: 'bootstrap-test', path: '/bootstrap-test', component: BootstrapTest },
         { name: 'test', path: '/test', component: HelloWorld },
     ],
 });
---
+++ ani_app-frontend/src/views/Index.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <b-carousel
+                id="carousel-1"
+                v-model="slide"
+                :interval="4000"
+                controls
+                indicators
+                @sliding-start="onSlideStart"
+                @sliding-end="onSlideEnd"
+        >
+            <b-carousel-slide
+                    caption="Потеряли питомца?"
+                    text="Моежете найти его здесь."
+                    img-src="/pets/cat1.jpg"
+            ></b-carousel-slide>
+            <b-carousel-slide
+                    caption="Нашли проавшего зверя?"
+                    text="Посмотрите здесь, может это он."
+                    img-src="/pets/dog2.jpg"
+            ></b-carousel-slide>
+            <b-carousel-slide
+                    caption="Зарегистрируйтесь"
+                    text="Или войдите, если есть учётная запись"
+                    img-src="/pets/cat3.jpg"
+            >
+                <b-button-toolbar class="d-flex justify-content-center">
+                    <b-button class="mx-1" variant="primary" href="#">Войти</b-button>
+                    <b-button class="mx-1" variant="primary" href="#">Зарегистрироваться</b-button>
+                </b-button-toolbar>
+            </b-carousel-slide>
+        </b-carousel>
+    </div>
+</template>
+
+<style scoped>
+div {
+    max-height: calc(100vh - 6vh);
+}
+</style>
+
+<script setup>
+import { ref } from 'vue';
+
+const slide = ref(0);
+const sliding = ref(false);
+
+function onSlideStart() {
+    sliding.value = true;
+}
+
+function onSlideEnd() {
+    sliding.value = false;
+}
+</script>
